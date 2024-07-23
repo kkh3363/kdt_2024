a977bca30925bd4142191d4d8e4ae3f5dcc23591
kkh3363 <kkh3363@gmail.com>
2024-07-23 14:57:23 +0900

    Add files via upload

---
+++ 강의자료/06_Database/예제코드/demo_madang_init.sql
@@ -0,0 +1,61 @@
+-- 이름: demo_madang_init.sql
+-- Madang 서점의 모든 실습 데이터를 초기화 한다.
+
+DROP TABLE if exists Orders;
+DROP TABLE if exists Book;
+DROP TABLE if exists Customer;
+
+
+CREATE TABLE Book (
+  bookid      INTEGER PRIMARY KEY,
+  bookname    VARCHAR(40),
+  publisher   VARCHAR(40),
+  price       INTEGER 
+);
+
+CREATE TABLE  Customer (
+  custid      INTEGER PRIMARY KEY,  
+  name        VARCHAR(40),
+  address     VARCHAR(50),
+  phone       VARCHAR(20)
+);
+
+
+CREATE TABLE Orders (
+  orderid INTEGER PRIMARY KEY,
+  custid  INTEGER REFERENCES Customer(custid),
+  bookid  INTEGER REFERENCES Book(bookid),
+  saleprice INTEGER,
+  orderdate DATE
+);
+
+-- Book, Customer, Orders 데이터 생성
+INSERT INTO Book VALUES(1, '축구의 역사', '굿스포츠', 7000);
+INSERT INTO Book VALUES(2, '축구 아는 여자', '나무수', 13000);
+INSERT INTO Book VALUES(3, '축구의 이해', '대한미디어', 22000);
+INSERT INTO Book VALUES(4, '골프 바이블', '대한미디어', 35000);
+INSERT INTO Book VALUES(5, '피겨 교본', '굿스포츠', 8000);
+INSERT INTO Book VALUES(6, '배구 단계별기술', '굿스포츠', 6000);
+INSERT INTO Book VALUES(7, '야구의 추억', '이상미디어', 20000);
+INSERT INTO Book VALUES(8, '야구를 부탁해', '이상미디어', 13000);
+INSERT INTO Book VALUES(9, '올림픽 이야기', '삼성당', 7500);
+INSERT INTO Book VALUES(10, 'Olympic Champions', 'Pearson', 13000);
+
+INSERT INTO Customer VALUES (1, '박지성', '영국 맨체스터', '000-5000-0001');
+INSERT INTO Customer VALUES (2, '김연아', '대한민국 서울', '000-6000-0001');  
+INSERT INTO Customer VALUES (3, '김연경', '대한민국 경기도', '000-7000-0001');
+INSERT INTO Customer VALUES (4, '추신수', '미국 클리블랜드', '000-8000-0001');
+INSERT INTO Customer VALUES (5, '박세리', '대한민국 대전',  NULL);
+
+INSERT INTO Orders VALUES (1, 1, 1, 6000, STR_TO_DATE('2024-07-01','%Y-%m-%d')); 
+INSERT INTO Orders VALUES (2, 1, 3, 21000, STR_TO_DATE('2024-07-03','%Y-%m-%d'));
+INSERT INTO Orders VALUES (3, 2, 5, 8000, STR_TO_DATE('2024-07-03','%Y-%m-%d')); 
+INSERT INTO Orders VALUES (4, 3, 6, 6000, STR_TO_DATE('2024-07-04','%Y-%m-%d')); 
+INSERT INTO Orders VALUES (5, 4, 7, 20000, STR_TO_DATE('2024-07-05','%Y-%m-%d'));
+INSERT INTO Orders VALUES (6, 1, 2, 12000, STR_TO_DATE('2024-07-07','%Y-%m-%d'));
+INSERT INTO Orders VALUES (7, 4, 8, 13000, STR_TO_DATE( '2024-07-07','%Y-%m-%d'));
+INSERT INTO Orders VALUES (8, 3, 10, 12000, STR_TO_DATE('2024-07-08','%Y-%m-%d')); 
+INSERT INTO Orders VALUES (9, 2, 10, 7000, STR_TO_DATE('2024-07-09','%Y-%m-%d')); 
+INSERT INTO Orders VALUES (10, 3, 8, 13000, STR_TO_DATE('2024-07-10','%Y-%m-%d'));
+
+COMMIT;
