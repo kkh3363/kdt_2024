f497bab8ef15c28bf3de5164f4daaa576f76ae4b
kkh3363 <kkh3363@gmail.com>
2024-08-07 16:58:03 +0900

    Create MethodDemo2.java

---
+++ 강의자료/07_Java/Source/제네릭타입/MethodDemo2.java
@@ -0,0 +1,36 @@
+package generic_package;
+
+
+public class MethodDemo {
+	static class Utils {
+		public static <T> void showArray(T[] a) {
+			for (T t : a)
+				System.out.printf("%s", t);
+			System.out.println();
+		}
+		//
+		public static <T extends Number> void showArrayNumber(T[] a) {
+			for (T t : a)
+				System.out.printf("%s", t);
+			System.out.println();
+		}
+		public static <T> T getLast(T[] a) {
+			return a[a.length-1];
+		}
+	}
+	
+	public static void main(String[] args) {
+		Integer[] ia = { 1, 2, 3, 4, 5 };
+		Character[] ca = { 'H', 'E', 'L', 'L', 'O' };
+
+		Utils.showArray(ia);
+		Utils.<Character>showArray(ca);
+		
+		Utils.showArrayNumber(ia);
+		//Utils.showArrayNumber(ca);
+
+		System.out.println(Utils.getLast(ia));
+
+	}
+
+}
