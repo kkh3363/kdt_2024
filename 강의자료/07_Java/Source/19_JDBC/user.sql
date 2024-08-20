3771ac566e5c7e83848f15b13602976683a83e31
kkh3363 <kkh3363@gmail.com>
2024-08-20 14:32:47 +0900

    Update user.sql

--- 강의자료/07_Java/Source/19_JDBC/user.sql
+++ 강의자료/07_Java/Source/19_JDBC/user.sql
@@ -4,4 +4,11 @@ grant all privileges on javadb.* to juser@localhost;
 commit;
 
 
+create table users (
+	userid   		varchar(50)	  	primary key, 
+	username		varchar(50)	  	not null,
+	userpassword	varchar(50)	  	not null,
+	userage			numeric(3)		not null,
+	useremail		varchar(50)	  	not null
+);
 
