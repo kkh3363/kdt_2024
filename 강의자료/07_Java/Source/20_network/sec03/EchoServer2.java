5ea0b93d67a4d70161a6b324439b0d87fa60c1d7
kkh3363 <kkh3363@gmail.com>
2024-08-21 10:57:54 +0900

    Create EchoServer2.java

---
+++ 강의자료/07_Java/Source/20_network/sec03/EchoServer2.java
@@ -0,0 +1,49 @@
+package sec03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class EchoServer2 extends Thread{
+	protected static boolean cont = true;
+    protected Socket connection = null;
+    
+    private EchoServer2(Socket clientSocket) {
+        connection = clientSocket;
+        start();
+    }
+    public void run() {
+        BufferedReader in;
+
+        System.out.println("클라이언트와 통신을 위한 새로운 스레드 생성");
+        try {
+            in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            String msg;
+
+            while ((msg = in.readLine()) != null) {
+                System.out.println("읽은 메시지 메아리 : " + msg);
+            }
+
+            in.close();
+            connection.close();
+        } catch (IOException e) {
+        }
+    }
+	public static void main(String[] args) throws IOException{
+		ServerSocket server = null;
+        server = new ServerSocket(5000);
+       
+        System.out.println("서버 소켓 생성");
+        while (cont) {
+        	System.out.println("연결 대기 중.....");
+            new EchoServer2(server.accept());
+        }
+       
+        server.close();
+
+	}
+
+}
