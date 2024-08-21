aacb856944836b9faf2c12f39a7f1b4b895e67de
kkh3363 <kkh3363@gmail.com>
2024-08-21 13:54:47 +0900

    Update UdpServer.java

--- 강의자료/07_Java/Source/20_network/sec04/UdpServer.java
+++ 강의자료/07_Java/Source/20_network/sec04/UdpServer.java
@@ -3,6 +3,7 @@ package sec04;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketAddress;
+import java.util.Scanner;
 
 public class UdpServer extends Thread {
 	private static DatagramSocket datagramSocket = null;
@@ -12,6 +13,15 @@ public class UdpServer extends Thread {
 		
 		//UDP 서버 시작
 		startServer();
+		//키보드 입력
+		Scanner scanner = new Scanner(System.in);
+		while(true) {
+			String key = scanner.nextLine();
+			if(key.toLowerCase().equals("q")) {
+				break;
+			}
+		}
+		scanner.close();
 		stopServer();
 	}
 	public static void startServer() {
@@ -19,20 +29,25 @@ public class UdpServer extends Thread {
 			@Override
 			public void run() {
 				try {
-					//클라이언트가 구독하고 싶은 뉴스 주제 얻기
-					DatagramPacket receivePacket = new DatagramPacket(new byte[1024], 1024);
-					datagramSocket.receive(receivePacket);
-					String newsKind = new String(receivePacket.getData(), 0, receivePacket.getLength(), "UTF-8");
-					
-					//클라이언트의 IP와 Port 얻기
-					SocketAddress socketAddress = receivePacket.getSocketAddress();
-
-					//10개의 뉴스를 클라이언트로 전송
-					for(int i=1; i<=10; i++) {
-						String data = newsKind + ": 뉴스" + i;
-						byte[] bytes = data.getBytes("UTF-8");
-						DatagramPacket sendPacket = new DatagramPacket(bytes, 0, bytes.length, socketAddress);
-						datagramSocket.send(sendPacket);
+					//DatagramSocket 생성 및 Port 바인딩
+					datagramSocket = new DatagramSocket(50001);
+					System.out.println( "[서버] 시작됨");
+					while ( true ) {
+						//클라이언트가 구독하고 싶은 뉴스 주제 얻기
+						DatagramPacket receivePacket = new DatagramPacket(new byte[1024], 1024);
+						datagramSocket.receive(receivePacket);
+						String newsKind = new String(receivePacket.getData(), 0, receivePacket.getLength(), "UTF-8");
+						
+						//클라이언트의 IP와 Port 얻기
+						SocketAddress socketAddress = receivePacket.getSocketAddress();
+	
+						//10개의 뉴스를 클라이언트로 전송
+						for(int i=1; i<=10; i++) {
+							String data = newsKind + ": 뉴스" + i;
+							byte[] bytes = data.getBytes("UTF-8");
+							DatagramPacket sendPacket = new DatagramPacket(bytes, 0, bytes.length, socketAddress);
+							datagramSocket.send(sendPacket);
+						}
 					}
 					
 				}catch (Exception e) {
