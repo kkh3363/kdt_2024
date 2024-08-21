dc16457d3464a767a4af9fcacf2348b2e62ba5d2
kkh3363 <kkh3363@gmail.com>
2024-08-21 09:35:48 +0900

    Create InetDemo2.java

---
+++ 강의자료/07_Java/Source/20_network/sec01/InetDemo2.java
@@ -0,0 +1,36 @@
+package sec01;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class InetDemo {
+
+	public static void main(String[] args) {
+		InetAddress addr1 = null, addr2 = null;
+		InetAddress[] addrs = null;
+		
+		System.out.print("호스트 이름을 입력하시오 : ");
+		Scanner in = new Scanner(System.in);
+		String url = in.nextLine();
+		
+		try {
+			addr1 = InetAddress.getByName(url);
+			addrs = InetAddress.getAllByName(url);
+			
+			addr2 = InetAddress.getLocalHost();
+		} catch (UnknownHostException e) {
+		}
+		
+		System.out.print(url + "의 IP 주소 : ");
+		System.out.println(addr1.getHostAddress());
+		System.out.print("로컬 IP 주소 : ");
+		System.out.println(addr2.getHostAddress());
+		
+		for(InetAddress remote : addrs) {
+			System.out.println(url+" IP 주소: " + remote.getHostAddress());
+		}
+
+	}
+
+}
