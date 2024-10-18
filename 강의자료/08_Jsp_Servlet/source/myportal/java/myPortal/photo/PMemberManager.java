2483778458c666450a8b21b7484a922828fa8689
김경훈 <kkh3363@gmail.com>
2024-10-18 16:00:10 +0900

    Update PMemberManager.java

--- 강의자료/08_Jsp_Servlet/source/myportal/java/myPortal/photo/PMemberManager.java
+++ 강의자료/08_Jsp_Servlet/source/myportal/java/myPortal/photo/PMemberManager.java
@@ -55,4 +55,53 @@ public class PMemberManager {
 		}
 		return flag;
 	}
+	// PMember Login
+		public boolean loginPMember(String id, String pwd) {
+			Connection con = null;
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+			String sql = null;
+			boolean flag = false;
+			try {
+				con = pool.getConnection();
+				sql = "select id from tblPMember where id=? and pwd=?";
+				pstmt = con.prepareStatement(sql);
+				pstmt.setString(1, id);
+				pstmt.setString(2, pwd);
+				rs = pstmt.executeQuery();
+				flag = rs.next();
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				pool.freeConnection(con, pstmt, rs);
+			}
+			return flag;
+		}
+
+		// PMember Get
+		public PMemberBean getPMember(String id) {
+			Connection con = null;
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+			String sql = null;
+			PMemberBean bean = new PMemberBean();
+			try {
+				con = pool.getConnection();
+				sql = "select name, pwd ,profile from tblPMember where id=?";
+				pstmt = con.prepareStatement(sql);
+				pstmt.setString(1, id);
+				rs = pstmt.executeQuery();
+				if (rs.next()) {
+					bean.setId(id);
+					bean.setName(rs.getString(1));
+					bean.setPwd(rs.getString(2));
+					bean.setProfile(rs.getString(3));
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				pool.freeConnection(con, pstmt, rs);
+			}
+			return bean;
+		}
 }
