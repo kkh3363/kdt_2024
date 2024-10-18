2ca53ed121fd8a403670e506326a4d1a0b2f9c2c
김경훈 <kkh3363@gmail.com>
2024-10-18 15:32:15 +0900

    Add files via upload

---
+++ 강의자료/08_Jsp_Servlet/source/myportal/java/myPortal/photo/PMemberBean.java
@@ -0,0 +1,35 @@
+package myPortal.photo;
+
+public class PMemberBean {
+	private String id;
+	private String pwd;
+	private String name;
+	private String profile;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getProfile() {
+		return profile;
+	}
+	public void setProfile(String profile) {
+		this.profile = profile;
+	}
+	
+	
+}
