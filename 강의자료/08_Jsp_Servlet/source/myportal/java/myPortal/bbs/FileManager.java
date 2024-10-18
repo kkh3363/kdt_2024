c580910daae4d4df9ed3cb5a7a6051f409a56719
김경훈 <kkh3363@gmail.com>
2024-10-18 14:54:23 +0900

    Update FileManager.java

--- 강의자료/08_Jsp_Servlet/source/myportal/java/myPortal/bbs/FileManager.java
+++ 강의자료/08_Jsp_Servlet/source/myportal/java/myPortal/bbs/FileManager.java
@@ -12,23 +12,23 @@ public class FileManager {
 	//private static final long serialVersionUID = 1L;
 	static final String CHARSET="UTF-8";
 	static final String ATTACHES_DIR = "c:\\java\\temp";
-	String fileName="";
+	public String fileName="";
 	int fileSize=0;
 	public void fileUpload( HttpServletRequest req) throws 
 			IOException, ServletException {
-		System.out.printf("파라미터 명");
+		
+		fileUpload(req,ATTACHES_DIR );
+	}
+	public void fileUpload( HttpServletRequest req, String targetDir) throws 
+	IOException, ServletException {
 		Collection<Part> parts = req.getParts();
 		for(Part part : parts) {
-			/*
-			 * System.out.printf("파라미터 명 : %s, contentType :  %s,  size : %d bytes \n",
-			 * part.getName(), part.getContentType(), part.getSize());
-			 */
 			fileSize = (int)part.getSize();
 			if(part.getHeader("Content-Disposition").contains("filename=")){
 				fileName= extractFileName(part.getHeader("Content-Disposition"));
 				if(part.getSize()>0){
 					//System.out.printf("업로드 파일 명 : %s  \n", fileName);
-					part.write(ATTACHES_DIR + File.separator + fileName);
+					part.write(targetDir + File.separator + fileName);
 					part.delete();
 				}
 			} 
