415c9299d148d73b84959f3004963a3613526c46
김경훈 <kkh3363@gmail.com>
2024-10-22 23:58:56 +0900

    Add files via upload

---
+++ 강의자료/08_Jsp_Servlet/리스너_필터/ListenerExam.java
@@ -0,0 +1,95 @@
+package myapp.listener;
+
+import jakarta.servlet.ServletContextAttributeEvent;
+import jakarta.servlet.ServletContextAttributeListener;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.http.HttpSessionAttributeListener;
+import jakarta.servlet.http.HttpSessionBindingEvent;
+import jakarta.servlet.http.HttpSessionEvent;
+import jakarta.servlet.http.HttpSessionListener;
+
+/**
+ * Application Lifecycle Listener implementation class ListenerExam
+ *
+ */
+public class ListenerExam implements HttpSessionListener, HttpSessionAttributeListener, ServletContextListener, ServletContextAttributeListener {
+
+    /**
+     * Default constructor. 
+     */
+    public ListenerExam() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see HttpSessionAttributeListener#attributeReplaced(HttpSessionBindingEvent)
+     */
+    public void attributeReplaced(HttpSessionBindingEvent se)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see HttpSessionListener#sessionCreated(HttpSessionEvent)
+     */
+    public void sessionCreated(HttpSessionEvent se)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see HttpSessionListener#sessionDestroyed(HttpSessionEvent)
+     */
+    public void sessionDestroyed(HttpSessionEvent se)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see HttpSessionAttributeListener#attributeRemoved(HttpSessionBindingEvent)
+     */
+    public void attributeRemoved(HttpSessionBindingEvent se)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletContextAttributeListener#attributeReplaced(ServletContextAttributeEvent)
+     */
+    public void attributeReplaced(ServletContextAttributeEvent scae)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletContextAttributeListener#attributeRemoved(ServletContextAttributeEvent)
+     */
+    public void attributeRemoved(ServletContextAttributeEvent scae)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent sce)  { 
+    	System.out.println("ServletContext 시작됨");
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent sce)  { 
+    	System.out.println("ServletContext 종료됨");
+    }
+
+	/**
+     * @see ServletContextAttributeListener#attributeAdded(ServletContextAttributeEvent)
+     */
+    public void attributeAdded(ServletContextAttributeEvent scae)  { 
+         System.out.println("ServletContext 속성 추가 : " +scae.getValue());
+    }
+
+	/**
+     * @see HttpSessionAttributeListener#attributeAdded(HttpSessionBindingEvent)
+     */
+    public void attributeAdded(HttpSessionBindingEvent se)  { 
+         // TODO Auto-generated method stub
+    }
+	
+}
---
+++ 강의자료/08_Jsp_Servlet/리스너_필터/ListenerTestServlet.java
@@ -0,0 +1,56 @@
+package myapp.ch11;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+/**
+ * Servlet implementation class ListenerTestServlet
+ */
+@WebServlet("/listenerTest")
+public class ListenerTestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	ServletContext sc;
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ListenerTestServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		sc = getServletContext();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		sc.setAttribute("scName", "홍길동");
+		HttpSession s = request.getSession();
+		s.setAttribute("ssName", s.getId()+": 세션 속성 저장!!");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
