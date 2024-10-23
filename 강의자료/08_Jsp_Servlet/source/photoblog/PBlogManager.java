d17db6bac0168659c682a396395c2b31fb256b51
김경훈 <kkh3363@gmail.com>
2024-10-23 14:30:51 +0900

    Update PBlogManager.java

--- 강의자료/08_Jsp_Servlet/source/photoblog/PBlogManager.java
+++ 강의자료/08_Jsp_Servlet/source/photoblog/PBlogManager.java
@@ -3,6 +3,7 @@ package myPortal.pblog;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.Vector;
 
 import myPortal.db.DBConnectionMgr;
 
@@ -39,5 +40,57 @@ public class PBlogManager {
 		}
 		return flag;
 		
+	}
+	// pMember Get
+	public PMemberBean getPMember(String id) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = null;
+		PMemberBean bean = new PMemberBean();
+		try {
+			con = pool.getConnection();
+			sql = "select name,profile from tblPMember where id=?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				bean.setId(id);
+				bean.setName(rs.getString(1));
+				bean.setProfile(rs.getString(2));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			pool.freeConnection(con, pstmt, rs);
+		}
+		return bean;
+	}
+	// Random PMember List - 본인을 제외한 5명 리스트
+	public Vector<PMemberBean> listPMember(String id) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = null;
+		Vector<PMemberBean> vlist = new Vector<PMemberBean>();
+		try {
+			con = pool.getConnection();
+			sql = "select id,name,profile from tblPMember where id !=? order by rand() limit 5";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				PMemberBean bean = new PMemberBean();
+				bean.setId(rs.getString(1));
+				bean.setName(rs.getString(2));
+				bean.setProfile(rs.getString(3));
+				vlist.addElement(bean);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			pool.freeConnection(con, pstmt, rs);
+		}
+		return vlist;
 	}
 }
