b6a1e2f46591763194ef8e51fc58f264b4f7e01f
김경훈 <kkh3363@gmail.com>
2024-10-25 13:58:24 +0900

    Update NewsDao.java

--- 강의자료/08_Jsp_Servlet/mvc/예제/뉴스그룹/NewsDao.java
+++ 강의자료/08_Jsp_Servlet/mvc/예제/뉴스그룹/NewsDao.java
@@ -3,8 +3,11 @@ package mymvc.models;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
+import myPortal.bbs.BoardBean;
 
 public class NewsDao {
 	private DBConnectionMgr pool;
@@ -15,6 +18,7 @@ public class NewsDao {
 			e.printStackTrace();
 		}
 	}
+	// 리스트 가져오기....
 	public List<News> getAll() throws Exception {
 		List<News> newsList = new ArrayList<>();
 		Connection con = null;
@@ -39,5 +43,31 @@ public class NewsDao {
 			pool.freeConnection(con, pstmt, rs);
 		}
 		return newsList;
+	}
+	//리스트 가져오기....
+	public News getNews(int aid) throws SQLException {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = null;
+		News newsBean = new News();
+		try {
+			con = pool.getConnection();
+			sql = "select aid, title, img, str_to_date(date,'%Y-%m-%d %h:%M:%s') as cdate, content from news where aid=?";
+			pstmt = con.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				newsBean.setAid(rs.getInt("aid"));
+				newsBean.setTitle(rs.getString("title"));
+				newsBean.setImg(rs.getString("img"));
+				newsBean.setDate(rs.getString("cdate"));
+				newsBean.setContent(rs.getString("content"));
+			}
+		}catch (Exception e) {
+				e.printStackTrace();
+		} finally {
+				pool.freeConnection(con, pstmt, rs);
+		}
+		return newsBean;
 	}
 }
