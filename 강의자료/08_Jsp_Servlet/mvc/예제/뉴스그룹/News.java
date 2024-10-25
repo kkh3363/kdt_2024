24001b0c5a64570920fe63c8bda07f051f6510d5
김경훈 <kkh3363@gmail.com>
2024-10-25 13:47:49 +0900

    Create News.java

---
+++ 강의자료/08_Jsp_Servlet/mvc/예제/뉴스그룹/News.java
@@ -0,0 +1,41 @@
+package mymvc.models;
+
+public class News {
+	private int aid;
+	private String title;
+	private String img;
+	private String date;
+	private String content;
+	
+	public int getAid() {
+		return aid;
+	}
+	public void setAid(int aid) {
+		this.aid = aid;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getImg() {
+		return img;
+	}
+	public void setImg(String img) {
+		this.img = img;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+}
