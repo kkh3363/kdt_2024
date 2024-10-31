d0f456993dc147210390d5b491d0f8d78950a21f
김경훈 <kkh3363@gmail.com>
2024-10-31 23:57:04 +0900

    Add files via upload

--- 강의자료/10_Spring/Source/ch01/Member/SpringConfig.java
+++ 강의자료/10_Spring/Source/ch01/Member/SpringConfig.java
@@ -1,14 +1,21 @@
 package com.myapp;
 
+import com.myapp.repository.JdbcMemberRepository;
 import com.myapp.repository.MemberRepository;
 import com.myapp.repository.MemoryMemberRepository;
 import com.myapp.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 
 public class SpringConfig {
+    private final DataSource dataSource;
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +23,7 @@ public class SpringConfig {
     }
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
