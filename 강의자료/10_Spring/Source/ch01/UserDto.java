ee1ecc02165b63064291526a27bb811ac86a6f6d
김경훈 <kkh3363@gmail.com>
2024-10-31 14:26:28 +0900

    Add files via upload

---
+++ 강의자료/10_Spring/Source/ch01/UserDto.java
@@ -0,0 +1,13 @@
+package com.myspring.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class UserDto {
+    private String name;
+    private Integer age;
+}
