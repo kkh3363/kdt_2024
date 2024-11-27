01fd48910740ca9b70807b70fb2b58958ba7dd33
김경훈 <kkh3363@gmail.com>
2024-11-27 16:06:33 +0900

    Update article.js

--- 강의자료/10_Spring/블로그 만들기/Source/template/js/article.js
+++ 강의자료/10_Spring/블로그 만들기/Source/template/js/article.js
@@ -1,16 +1,96 @@
+
+const createButton = document.getElementById('create-btn');
+
+if (createButton ){
+    createButton.addEventListener('click', event =>{
+        body = JSON.stringify({
+            title: document.getElementById('title').value,
+            content :document.getElementById('content').value
+        });
+        function success(){
+            alert("등록 완료 되었습니다.");
+            location.replace('/articles');
+        };
+        function fail(){
+            alert("등록 실패 !!!");
+            location.replace('/articles');
+        };
+        httpRequest('POST', '/api/articles', body, success, fail);
+    });
+}
+
+function getCookie(key){
+    var result = null;
+    var cookie = document.cookie.split(';');
+    cookie.some(function (item){
+        item = item.replace(' ','');
+        var dic = item.split('=');
+        if (key === dic[0]){
+            result = dic[1];
+            return true;
+        }
+    });
+    return result;
+}
+
+// HTTP 요청을 보내는 함수
+function httpRequest(method, url, body, success, fail) {
+    fetch(url, {
+        method: method,
+        headers: { // 로컬 스토리지에서 액세스 토큰 값을 가져와 헤더에 추가
+            Authorization: 'Bearer ' + localStorage.getItem('access_token'),
+            'Content-Type': 'application/json',
+        },
+        body: body,
+    }).then(response => {
+        if (response.status === 200 || response.status === 201) {
+            return success();
+        }
+        const refresh_token = getCookie('refresh_token');
+        if (response.status === 401 && refresh_token) {
+            fetch('/api/token', {
+                method: 'POST',
+                headers: {
+                    Authorization: 'Bearer ' + localStorage.getItem('access_token'),
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    refreshToken: getCookie('refresh_token'),
+                }),
+            })
+                .then(res => {
+                    if (res.ok) {
+                        return res.json();
+                    }
+                })
+                .then(result => { // 재발급이 성공하면 로컬 스토리지값을 새로운 액세스 토큰으로 교체
+                    localStorage.setItem('access_token', result.accessToken);
+                    httpRequest(method, url, body, success, fail);
+                })
+                .catch(error => fail());
+        } else {
+            return fail();
+        }
+    });
+}
+
 // 삭제 기능
 const deleteButton = document.getElementById('delete-btn');
 
 if (deleteButton != null) {
     deleteButton.addEventListener('click', event => {
         let id = document.getElementById('article-id').value;
-        fetch('/api/articles/' + id, {
-            method: 'DELETE'
-        })
-            .then((response) => {
-                alert('삭제가 완료되었습니다.');
-                location.replace('/articles');
-            });
+        function success() {
+            alert('삭제가 완료되었습니다.');
+            location.replace('/articles');
+        }
+
+        function fail() {
+            alert('삭제 실패했습니다.');
+            location.replace('/articles');
+        }
+
+        httpRequest('DELETE','/api/articles/' + id, null, success, fail);
     });
 }
 
@@ -22,41 +102,21 @@ if (modifyButton != null) {
         let params = new URLSearchParams(location.search);
         let id = params.get('id');
 
-        fetch('/api/articles/' + id, {
-            method: 'PUT',
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                title: document.getElementById('title').value,
-                content: document.getElementById('content').value
-            })
+        body = JSON.stringify({
+            title: document.getElementById('title').value,
+            content: document.getElementById('content').value
         })
-            .then((response) => {
-                alert('수정이 완료되었습니다.');
-                location.replace('/articles/' + id);
-            });
-    });
-}
 
-// 생성 기능
-const createButton = document.getElementById('create-btn');
+        function success() {
+            alert('수정 완료되었습니다.');
+            location.replace('/articles/' + id);
+        }
 
-if (createButton != null) {
-    createButton.addEventListener('click', event => {
-        fetch('/api/articles', {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                title: document.getElementById('title').value,
-                content: document.getElementById('content').value
-            })
-        })
-            .then((response) => {
-                alert('등록 완료되었습니다.');
-                location.replace('/articles');
-            });
+        function fail() {
+            alert('수정 실패했습니다.');
+            location.replace('/articles/' + id);
+        }
+
+        httpRequest('PUT','/api/articles/' + id, body, success, fail);
     });
 }
