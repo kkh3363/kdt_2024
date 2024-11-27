a51a347dee0996a47f3fb5d63c88f47545824994
김경훈 <kkh3363@gmail.com>
2024-11-27 14:10:37 +0900

    Add files via upload

---
+++ 강의자료/10_Spring/블로그 만들기/Source/OAuth2AuthorizationRequestBasedOnCookieRepository.java
@@ -0,0 +1,43 @@
+package com.kdt.myblog.config.oauth;
+
+import com.kdt.myblog.util.CookieUtil;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
+import org.springframework.web.util.WebUtils;
+
+public class OAuth2AuthorizationRequestBasedOnCookieRepository implements
+        AuthorizationRequestRepository<OAuth2AuthorizationRequest >
+{
+    public final static  String OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME="oauth2_auth_request";
+    private  final static int COOKIE_EXPIRE_SECONDS = 18000;  // 300분
+    @Override
+    public OAuth2AuthorizationRequest loadAuthorizationRequest(HttpServletRequest request) {
+        Cookie cookie = WebUtils.getCookie(request ,OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME);
+        return  CookieUtil.deserialize(cookie, OAuth2AuthorizationRequest.class);
+    }
+
+    @Override
+    public void saveAuthorizationRequest(OAuth2AuthorizationRequest authorizationRequest, HttpServletRequest request, HttpServletResponse response) {
+        if (authorizationRequest == null){
+            removeAuthorizationRequestCookie( request, response);
+            return;
+        }
+        CookieUtil.addCookie(response,
+                OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME,
+                CookieUtil.serialize(authorizationRequest),
+                COOKIE_EXPIRE_SECONDS
+        );
+    }
+
+    @Override
+    public OAuth2AuthorizationRequest removeAuthorizationRequest(HttpServletRequest request, HttpServletResponse response) {
+        return this.loadAuthorizationRequest(request);
+    }
+
+    public void removeAuthorizationRequestCookie(HttpServletRequest request, HttpServletResponse response){
+        CookieUtil.deleteCookie( request, response, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME);
+    }
+}
