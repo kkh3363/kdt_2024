95e580abf374730c3a6725960830cc740feb306f
김경훈 <kkh3363@gmail.com>
2024-12-12 11:06:02 +0900

    Update AppStyle3.js

--- 강의자료/11_React/Source/ch04_styled_components/AppStyle3.js
+++ 강의자료/11_React/Source/ch04_styled_components/AppStyle3.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled,{css} from 'styled-components'
 
 const StyledButton = styled.button`
   padding: 6px 12px;
@@ -17,11 +17,34 @@ function Button({ children, color, background }) {
       </StyledButton>
     );
   }
+  const StyledButton2 = styled.button`
+  padding: 6px 12px;
+  border-radius: 8px;
+  font-size: 1rem;
+  line-height: 1.5;
+  border: 1px solid lightgray;
 
+  ${ (props)  =>{
+        props.primary && 
+        css`
+            color: white;
+            background: navy;
+            border-color: navy;
+        `}
+    }
+`;
+  function Button2({ children, ...props }) {
+    return (
+      <StyledButton2 {...props} Î>
+        {children}
+      </StyledButton2>
+    );
+  }
 function AppStyle3(){
     return (
         <div>
-            <Button color="green" background="pink">  Green Button </Button>;
+            <Button color="green" background="pink">  Green Button </Button>
+            <Button2 primary>  Primary Button </Button2>
         </div>
     )
 }
