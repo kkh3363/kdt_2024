e2776762d8363de4ed9064ca65bcda4c5eac6f91
김경훈 <kkh3363@gmail.com>
2024-12-12 11:28:32 +0900

    Update AppStyle3.js

--- 강의자료/11_React/Source/ch04_styled_components/AppStyle3.js
+++ 강의자료/11_React/Source/ch04_styled_components/AppStyle3.js
@@ -8,44 +8,44 @@ const StyledButton = styled.button`
   border: 1px solid lightgray;
 
   color: ${(props) => props.color || "gray"};
-  background: ${(props) => props.background || "white"};
+  background-color: ${(props) => props.background || "white"};
 `;
 function Button({ children, color, background }) {
     return (
-      <StyledButton color={color} background={background} Î>
+      <StyledButton color={color} background={background} >
         {children}
       </StyledButton>
     );
   }
   const StyledButton2 = styled.button`
-    padding: 6px 12px;
+     padding: 6px 12px;
     border-radius: 8px;
     font-size: 1rem;
     line-height: 1.5;
     border: 1px solid lightgray;
-    color: ${(props) => props.color || "gray"};
-    background: ${(props) => props.background || "white"};
-    ${ (props)  =>{
-            props.primary && 
-            css`
-                color: white;
-                background: navy;
-                border-color: navy;
-            `}
-        }
+
+    color: ${(props) => props.color || 'gray'};
+    background: ${(props) => props.background || 'white'};
+
+    ${(props) =>
+        props.primary &&
+        css`
+            color: white;
+            background: navy;
+            border-color: navy;
+        `}
     `;
-    
+
   function Button2({ children, ...props }) {
     return (
-      <StyledButton2 {...props} Î>
-        {children}
-      </StyledButton2>
+      <StyledButton2 {...props} >{children}</StyledButton2>
     );
   }
 function AppStyle3(){
     return (
         <div>
             <Button color="green" background="pink">  Green Button </Button>
+            <Button2 >  No Primary Button </Button2>
             <Button2 primary>  Primary Button </Button2>
         </div>
     )
