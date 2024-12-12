355f7e3d91eb4279627cce4dc7e29aa00a8cc325
김경훈 <kkh3363@gmail.com>
2024-12-13 01:29:43 +0900

    Create Student.js

---
+++ 강의자료/11_React/Source/06_reducer/Student.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Student = ({ name, dispatch, id, isHere }) => {
+  return (
+    <div>
+      <span
+        style={{
+          textDecoration: isHere ? "line-through" : "none",
+          color: isHere ? "gray" : "black",
+        }}
+        onClick={() => {
+          dispatch({ type: "mark-student", payload: { id } });
+        }}
+      >
+        {name}
+      </span>
+      <button
+        onClick={() => {
+          dispatch({ type: "delete-student", payload: { id } });
+        }}
+      >
+        삭제
+      </button>
+    </div>
+  );
+};
+
+export default Student;
