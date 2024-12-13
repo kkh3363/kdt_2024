33fe2b9ae3fc0114320d75c8574749bb9f3cb66c
김경훈 <kkh3363@gmail.com>
2024-12-13 16:02:15 +0900

    Create App_skel.js

---
+++ 강의자료/11_React/Source/ch07_Redux/App_skel.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Counter(){
+    return (
+        <div>
+            <button >+</button> 0
+        </div>
+    )
+}
+function App(){
+    return (
+        <div>
+            <h1>증감기</h1>
+            <Counter></Counter>
+        </div>
+
+    )
+}
+export default App;
