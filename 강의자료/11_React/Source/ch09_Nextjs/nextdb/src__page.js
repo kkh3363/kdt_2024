b27996a75c4e1596c4144bf97074b8254977f2aa
김경훈 <kkh3363@gmail.com>
2024-12-23 10:16:21 +0900

    Update src__page.js

--- 강의자료/11_React/Source/ch09_Nextjs/nextdb/src__page.js
+++ 강의자료/11_React/Source/ch09_Nextjs/nextdb/src__page.js
@@ -1,26 +1,25 @@
 import Image from "next/image";
-import executeQuery from "./_lib/db.tsx";
+import executeQuery from "./_lib/db";
+
 export default async function Home() {
-  const sql = "select * from jspdb.tblmember";
-  const data = await executeQuery(sql, "");
-  const getdata = JSON.parse(JSON.stringify(data));
-  console.log(getdata);
-  return (
-    <>
-      <h2>Welcome</h2>
-      Hello, WEB
-      <p>
-        {getdata.map((data, i) => {
-          return (
-            <div>
-              <p>
-                {data.id} {data.name}
-              </p>
-            </div>
-          );
-        })}
-      </p>
-    </>
-  );
+  const sql = "select * from nextdb.member";
+  const data = await executeQuery(sql, "");
+  const getData = JSON.parse(JSON.stringify(data));
+  return (
+    <>
+      <h2>Welcome</h2>
+      Hello, WEB
+      <p>
+        {getData.map((data, i) => {
+          return (
+            <div key={i}>
+              <p>
+                {data.id} {data.name}
+              </p>
+            </div>
+          );
+        })}
+      </p>
+    </>
+  );
 }
-
