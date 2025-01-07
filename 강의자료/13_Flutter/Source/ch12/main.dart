87cddc4ddf6a29321c36b7ac057e28d350937be7
김경훈 <kkh3363@gmail.com>
2025-01-07 15:21:05 +0900

    Create main.dart

---
+++ 강의자료/13_Flutter/Source/ch12/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:vi_player/screen/home_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    ),
+  );
+}
