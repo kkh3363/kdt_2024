ab4bd13c15a71f8c83b2939ab03481b80c883d10
김경훈 <kkh3363@gmail.com>
2025-01-08 17:48:32 +0900

    Create ShowDialog.dart

---
+++ 강의자료/13_Flutter/ShowDialog.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class HomeDialog extends StatelessWidget {
+  const HomeDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('showDialog Sample')),
+      body: Center(
+        child: OutlinedButton(
+          onPressed: () => _dialogBuilder(context),
+          child: const Text('Open Dialog'),
+        ),
+      ),
+    );
+
+  }
+  Future<void> _dialogBuilder(BuildContext context) {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('알림창.....'),
+          content: const Text(
+            '다이얼 로그 에제.....',
+          ),
+          actions: <Widget>[
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              child: const Text('Disable'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              child: const Text('Enable'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
